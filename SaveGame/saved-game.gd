86134d30af2cbf709efafffb0a3a530837d4fa78
Rordigo <digoanater@gmail.com>
2024-11-03 18:22:40 -0300

    created basic savegame and section colors into board

---
+++ SaveGame/saved-game.gd
@@ -0,0 +1,6 @@
+class_name SavedGame
+extends Resource
+
+@export var sections : Array[SectionData]
+@export var pattern : TileMapPattern
+@export var pattern_offset : Vector2i
---
+++ SaveGame/saver-loader.gd
@@ -0,0 +1,29 @@
+class_name SaverLoader
+extends Node
+
+const SAVE_PATH = "user://save_file.tres"
+
+@onready var planet_board: PlanetBoard = %PlanetBoard
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("num9"):
+		save_game()
+	elif event.is_action_released("num0"):
+		load_game()
+
+func save_game() -> void:
+	var saved_game := SavedGame.new()
+
+	saved_game.pattern = planet_board.get_pattern()
+	saved_game.pattern_offset = planet_board.get_patern_offset()
+	saved_game.sections = planet_board.sections
+	
+	ResourceSaver.save(saved_game, SAVE_PATH)
+
+func load_game() -> void:
+	var saved_game: SavedGame = ResourceLoader.load(SAVE_PATH) as SavedGame
+	if saved_game == null:
+		return
+
+	planet_board.load_game(saved_game)
