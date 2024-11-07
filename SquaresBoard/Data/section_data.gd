850fd6f2fb3e03e54103788a045a705460da2a0c
Rordigo <digoanater@gmail.com>
2024-11-07 16:09:40 -0300

    load save section on open

---
+++ SquaresBoard/Data/section_data.gd
@@ -0,0 +1,11 @@
+class_name SectionData
+extends Resource
+
+@export var world_head_cell: Vector2i
+@export var width: int
+@export var height: int
+
+@export var finished: bool = false
+@export var pattern_offset: Vector2i
+@export var pattern: TileMapPattern
+@export var groups: Array[SectionColorGroup]
