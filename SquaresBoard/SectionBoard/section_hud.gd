c5c5afdd380b5838c088273582721ba0e4f02872
Rordigo <digoanater@gmail.com>
2024-11-07 12:40:54 -0300

    Added margin to section results panel

--- SquaresBoard/SectionBoard/section_hud.gd
+++ SquaresBoard/SectionBoard/section_hud.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-@onready var stats_container: VBoxContainer = $PanelContainer/StatsVBoxContainer
+@onready var stats_container: VBoxContainer = $PanelContainer/MarginContainer/StatsVBoxContainer
 @onready var pieces_placement_layer: PiecesPlacementLayer = $"../PiecesPlacementLayer"
 
 func _ready() -> void:
@@ -15,5 +15,5 @@ func show_stats(stats: Array[SectionColorGroup]) -> void:
 	var labels: Array[Label] = []
 	for group in stats:
 		var stat_label = Label.new()
-		stat_label.text = "   " + group.color + " - " + str(group.cells_count) + "   "
+		stat_label.text = group.color + " - " + str(group.cells_count)
 		stats_container.add_child(stat_label)
